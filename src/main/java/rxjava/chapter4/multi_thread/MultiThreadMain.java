0f91875a3b7cce2b4b1ed3e1d74d1f01cf6201f0
Young Jun Park <june0313@gmail.com>
2017-11-16 00:22:36 +0900

    RxJava의 멀티쓰레딩

---
+++ src/main/java/rxjava/chapter4/multi_thread/MultiThreadMain.java
@@ -0,0 +1,150 @@
+package rxjava.chapter4.multi_thread;
+
+import lombok.extern.slf4j.Slf4j;
+import rx.Observable;
+import rx.Scheduler;
+import rx.schedulers.Schedulers;
+import rxjava.chapter4.concurrency.SleepUtil;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class MultiThreadMain {
+    ExecutorService poolA = Executors.newFixedThreadPool(10, threadFactory("Scheduler-A"));
+    ExecutorService poolB = Executors.newFixedThreadPool(10, threadFactory("Scheduler-B"));
+    ExecutorService poolC = Executors.newFixedThreadPool(10, threadFactory("Scheduler-C"));
+
+    Scheduler schedulerA = Schedulers.from(poolA);
+    Scheduler schedulerB = Schedulers.from(poolB);
+    Scheduler schedulerC = Schedulers.from(poolC);
+
+
+    public static void main(String[] args) {
+        MultiThreadMain main = new MultiThreadMain();
+//        main.immediate();
+//        main.trampoline();
+//        main.subscribeOn();
+//        main.observeOn();
+//        main.observeOn2();
+        main.delay();
+        main.sleepOneSecond();
+    }
+
+    void immediate() {
+        Scheduler scheduler = Schedulers.immediate();
+        Scheduler.Worker worker = scheduler.createWorker();
+
+        log.info("Main start");
+        worker.schedule(() -> {
+            log.info(" Outer start");
+            sleepOneSecond();
+            worker.schedule(() -> {
+                log.info("  Inner start");
+                sleepOneSecond();
+                log.info("  Inner end");
+            });
+            log.info(" Outer end");
+        });
+        log.info("Main end");
+        worker.unsubscribe();
+    }
+
+    void trampoline() {
+        Scheduler scheduler = Schedulers.trampoline();
+        Scheduler.Worker worker = scheduler.createWorker();
+
+        log.info("Main start");
+        worker.schedule(() -> {
+            log.info(" Outer start");
+            sleepOneSecond();
+            worker.schedule(() -> {
+                log.info("  Inner start");
+                sleepOneSecond();
+                log.info("  Inner end");
+            });
+            log.info(" Outer end");
+        });
+        log.info("Main end");
+        worker.unsubscribe();
+    }
+
+    void subscribeOn() {
+        log.info("Starting");
+        final Observable<String> obs = simple();
+        log.info("Created");
+        obs.subscribeOn(Schedulers.computation())
+                .map(String::toLowerCase)
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                        x -> log.info("Got " + x),
+                        Throwable::printStackTrace,
+                        () -> log.info("Completed"));
+        log.info("Exiting");
+    }
+
+    void observeOn() {
+        log.info("Starting");
+        final Observable<String> obs = simple();
+        log.info("Created");
+        obs
+                .doOnNext(x -> log.info("Found 1 : " + x))
+                .observeOn(Schedulers.io())
+                .doOnNext(x -> log.info("Found 2 : " + x))
+                .subscribe(
+                        x -> log.info("Got 1: " + x),
+                        Throwable::printStackTrace,
+                        () -> log.info("Completed")
+                );
+        log.info("Exiting");
+    }
+
+    void observeOn2() {
+        log.info("Starting");
+        final Observable<String> obs = simple();
+        log.info("Created");
+        obs
+                .doOnNext(x -> log.info("Found 1 : " + x))
+                .observeOn(schedulerA)
+                .doOnNext(x -> log.info("Found 2 : " + x))
+                .observeOn(schedulerB)
+                .doOnNext(x -> log.info("Found 3 : " + x))
+                .subscribeOn(schedulerC)
+                .subscribe(
+                        x -> log.info("Got 1: " + x),
+                        Throwable::printStackTrace,
+                        () -> log.info("Completed")
+                );
+        log.info("Exiting");
+    }
+
+    void delay() {
+        Observable.just("A", "B")
+                .delay(500, TimeUnit.MILLISECONDS, schedulerA)
+                .subscribe(log::info);
+    }
+
+    private Observable<String> simple() {
+        return Observable.create(subscriber -> {
+            log.info("subscribed");
+            subscriber.onNext("A");
+            subscriber.onNext("B");
+            subscriber.onCompleted();
+        });
+    }
+
+    private void sleepOneSecond() {
+        SleepUtil.sleep(1000);
+    }
+
+    private ThreadFactory threadFactory(String name) {
+        return r -> {
+            Thread thread = new Thread(r);
+            thread.setName(name + "-" + thread.getId());
+            return thread;
+        };
+    }
+
+}
---
+++ src/main/java/rxjava/chapter4/multi_thread/RxGroceries.java
@@ -0,0 +1,55 @@
+package rxjava.chapter4.multi_thread;
+
+import lombok.extern.slf4j.Slf4j;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+import rxjava.chapter4.concurrency.SleepUtil;
+
+import java.math.BigDecimal;
+
+@Slf4j
+public class RxGroceries {
+    public static void main(String[] args) {
+        RxGroceries rxGroceries = new RxGroceries();
+        rxGroceries.getTotalPrice3().subscribe();
+        SleepUtil.sleep(10000);
+    }
+
+
+    Observable<BigDecimal> getTotalPrice() {
+        return Observable
+                .just("bread", "butter", "milk", "tomato", "cheese")
+                .subscribeOn(Schedulers.io())
+                .map(prod -> doPurchase(prod, 1))
+                .reduce(BigDecimal::add)
+                .single();
+    }
+
+    Observable<BigDecimal> getTotalPrice2() {
+        return Observable
+                .just("bread", "butter", "milk", "tomato", "cheese")
+                .subscribeOn(Schedulers.io())
+                .flatMap(prod -> purchase(prod, 1))
+                .reduce(BigDecimal::add)
+                .single();
+    }
+
+    Observable<BigDecimal> getTotalPrice3() {
+        return Observable
+                .just("bread", "butter", "milk", "tomato", "cheese")
+                .flatMap(prod -> purchase(prod, 1).subscribeOn(Schedulers.io()))
+                .reduce(BigDecimal::add)
+                .single();
+    }
+
+    Observable<BigDecimal> purchase(String productName, int quantity) {
+        return Observable.fromCallable(() -> doPurchase(productName, quantity));
+    }
+
+    private BigDecimal doPurchase(String productName, int quantity) {
+        log.info("Purchasing " + quantity + " " + productName);
+        SleepUtil.sleep(1000);
+        log.info("Done " + quantity + " " + productName);
+        return new BigDecimal("1");
+    }
+}
